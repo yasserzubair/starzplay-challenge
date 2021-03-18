1f84b8261631ef5e4efbc631a88cf2cbcf91d48f
Yasser Zubair <yasserzubair68@gmail.com>
2021-03-18 16:52:28 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import HeroCarousel from "./components/HeroCarousel";
+import Movies from "./containers/Movies";
+import TVShows from "./containers/TVShows";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/movies">
+            <Movies />
+          </Route>
+          <Route exact path="/tv-shows">
+            <TVShows />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
